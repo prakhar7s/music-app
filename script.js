a9f9be1ac0cd0a0bae6c7ef3d8256f6f247d7f47
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2021-01-29 00:59:12 +0530

    styles modified

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 import { songsNameList } from "./songs-name.js";
 
-const audio = document.querySelector("audio");
+// targets
 const songsList = document.querySelector("#songs-list");
-// const playButton = document.querySelector(".play-button");
-const currentSong = document.querySelector("#current-song");
+const playingSongName = document.querySelector("#song-name");
+const playingSongMovie = document.querySelector(".song-movie");
+const playingSongCover = document.querySelector(".song-cover-image");
 const pointer = document.querySelector("#pointer");
 
 // Songs
@@ -17,63 +18,79 @@ for (songName of songsNameList) {
   const song = {
     songID,
     songName,
-    song: new Audio(`${songName}.mp3`),
+    song: new Audio(`./assets/mp3/${songName}.mp3`),
+    interwal: null,
   };
   songs.push(song);
   createSongItem(song);
 }
 
-var runningTime;
-
+// bind it to windows bacause using module based javascript
 window.playSong = playSong;
 
 function playSong(event) {
+  // clicked song
   const clickedSong = songs.filter((song) => {
     return event.target.parentNode.parentNode.classList[0] === song.songID;
   });
 
+  // if song is playing
   if (event.target.classList[1] === "fa-pause") {
     clickedSong[0].song.pause();
-    event.target.className = "fa fa-play";
-    clearInterval(runningTime);
+    event.target.className = "fa fa-play"; //change icon
+    clearInterval(clickedSong[0].interwal); //clear the interwal
   } else {
+    // if song is not playing
+    // pause all song
     songs.map((song) => {
       song.song.pause();
+      clearInterval(song.interwal); //clear all interwals
+      song.interwal = null;
     });
 
+    // clear playing styles from all songs
     Array.from(songsList.childNodes).map((songItem) => {
       songItem.childNodes[2].firstChild.className = "fa fa-play";
       songItem.classList.remove("active");
     });
 
+    // apply playing styles only to clicked song
     event.target.parentNode.parentNode.classList.add("active");
 
+    //play the song
     clickedSong[0].song.play();
 
-    function s() {
+    // add pause icon because song is playing
+    event.target.className = "fa fa-pause";
+
+    clickedSong[0].interwal = setInterval(function () {
       const { currentTime, duration } = clickedSong[0].song;
       pointer.style.left = `${(100 / duration) * currentTime}%`;
-      console.log(currentTime, duration);
 
       if (currentTime >= duration) {
-        clearInterval(runningTime);
+        clearInterval(clickedSong[0].interwal);
       }
-    }
+    }, 1000);
 
-    runningTime = setInterval(s, 1000);
+    // add all info in player about the clicked song
+    const songName = clickedSong[0].songName.replaceAll("-", " ");
+    playingSongName.innerHTML = `${songName}`;
+    playingSongMovie.innerHTML = `${songName}`;
 
-    currentSong.childNodes[1].innerHTML = `${clickedSong[0].songName}`;
-
-    event.target.className = "fa fa-pause";
+    playingSongCover.src = `${
+      "./assets/cover/" + clickedSong[0].songName + ".jpg"
+    }`;
   }
 }
 
 function createSongItem(song) {
   const { songID, songName } = song;
 
-  songsList.innerHTML += `<div class='${songID} song-item'><p>${songName}</p>
+  songsList.innerHTML += `<div class='${songID} song-item'><p class="song-list-name">${songName}</p>
             <button onclick="playSong(event)"><i class="fa fa-play" aria-hidden="true"></i>
             </button>
-            <div class="song-cover"><img src=${songName + ".jpg"} /></div>
+            <div class="song-cover"><img src=${
+              "./assets/cover/" + songName + ".jpg"
+            } /></div>
         </div>`;
 }
